0b817a8f7c6454db2642b6090bcf1ed371932948
Vinicius <viniciusgclaco@gmail.com>
2025-10-21 18:48:20 -0300

    Dark mode

--- pages/login/login.js
+++ pages/login/login.js
@@ -1,22 +1,3 @@
-const itens = document.querySelectorAll('.card');
-
-function fadeIn() {
-    itens.forEach((item, index) => {
-        setTimeout(() => {
-            item.classList.add('mostrar');
-        }, index * 400);
-    });
-}
-
-window.addEventListener('scroll', function () {
-    if (window.scrollY >= 300) {
-        fadeIn();
-
-    // Para que a função só seja chamada apenas uma vez
-        window.removeEventListener('scroll', arguments.callee);
-    }
-});
-
 let trilho = document.getElementById("trilho");
 let body = document.querySelector("body");
 let header = document.querySelector("header");
