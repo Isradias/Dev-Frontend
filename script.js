476c1d342868eacb1bfced7e90b53ecff6468762
Isradias <israelcaldas10@gmail.com>
2025-10-21 09:52:02 -0300

    Todos os header atualizados com o botão de dark mode e alterações que foram feitas no index (exclusão de tópicos desnecessários), css ainda precisa atualizar

--- script.js
+++ script.js
@@ -15,4 +15,14 @@ window.addEventListener('scroll', function () {
     // Para que a função só seja chamada apenas uma vez
         window.removeEventListener('scroll', arguments.callee);
     }
-  });
+});
+
+let trilho = document.getElementById("trilho");
+let body = document.querySelector("body");
+let header = document.querySelector("header");
+
+trilho.addEventListener("click", ()=>{
+    trilho.classList.toggle("dark");
+    body.classList.toggle("dark");
+    header.classList.toggle("dark");
+})
